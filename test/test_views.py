a35cb3ab20178b9074fe98cdbc83960002391232
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-03-22 10:37:06 +0100

    Poprawka JSONa

--- test/test_views.py
+++ test/test_views.py
@@ -2,7 +2,6 @@ import unittest
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
-
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
