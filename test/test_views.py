8175a3d9cea1144e430b9d99858a02085b78cd23
naitmerous <fejbian21@gmail.com>
2025-05-08 15:51:59 +0200

    veiwsy3

--- test/test_views.py
+++ test/test_views.py
@@ -10,7 +10,7 @@ class FlaskrTestCase(unittest.TestCase):
     def test_outputs(self):
         rv = self.app.get('/outputs')
         s = rv.data.decode()
-        self.assertIn(','.join(SUPPORTED), s)
+        self.assertIn(', '.join(SUPPORTED), s)
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
